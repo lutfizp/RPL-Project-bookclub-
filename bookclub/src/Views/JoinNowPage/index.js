9092d369a00b01b2f000150e776b6e9acc8931e2
fanina huwaida <fanina0105@gmail.com>
2024-03-06 22:16:29 +0700

    pushh

--- bookclub/src/Views/JoinNowPage/index.js
+++ bookclub/src/Views/JoinNowPage/index.js
@@ -11,7 +11,7 @@ function JoinNowPage() {
 
   return (
     <div className="App">
-      <div className="navbar flex bg-amber-50 fixed z-25 rounded-b-lg w-full">
+      <div className="navbar flex fixed z-10 rounded-b-lg w-full" style={{ backgroundColor: '#C8AE7D' }}>
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1, x: 20 }}
@@ -20,20 +20,21 @@ function JoinNowPage() {
         >
           <motion.a
            href="/About"
-            className="px-2 group hover:text-white"
+            className="px-2 group hover:text-yellow-950"
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 1.3 }}
             transition={{ ease: "easeOut", duration: 0.3 }}
             style={{fontFamily: 'kaisei tokumin'}}
           >
+            Home
             About
             <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-white"></span>
             
           </motion.a>
           <div className="divider divider-horizontal bg-white w-auto"></div>
           <motion.a
-            href="FAQ"            
-            className="px-2 group hover:text-white"
+            href="FAQ"
+            className="px-2 group text-yellow-950"
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 1.3 }}
             transition={{ ease: "easeOut", duration: 0.3 }}
@@ -45,7 +46,7 @@ function JoinNowPage() {
           <div className="divider divider-horizontal bg-white w-auto"></div>
           <motion.a
             href="/Contact Us"
-            className="px-2 group hover:text-white"
+            className="px-2 group hover:text-yellow-950"
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 1.3 }}
             transition={{ ease: "easeOut", duration: 0.3 }}
@@ -55,20 +56,30 @@ function JoinNowPage() {
             <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-white"></span>
           </motion.a>
           <div className="divider divider-horizontal bg-white w-auto"></div>
+          <motion.a
+            href="JoinPage"
+            className="px-2 group hover:text-yellow-950"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 1.3 }}
+            transition={{ ease: "easeOut", duration: 0.3 }}
+            style={{fontFamily: 'kaisei tokumin'}}
+          
+          >
+          </motion.a>
         </motion.div>
       </div>
-        <div className="hero-content flex-col lg:flex-col">
-          <div className="mt-40 sm:mt-16">
-            <h1 className="sm:max-w-sm sm:max-h-sm text-5xl font-bold">
+      <div className="hero-content flex-col lg:flex-row" style={{ marginTop: '-100px' }}>
+          <div className="md:pr-20 animate-fade-left" style={{ flex: 30 }}>
+            <h1 className="sm:max-w-sm sm:max-h-sm text-4xl font-bold animate-fade-up text-center" style={{ fontFamily: 'Tinos', color:"#484848", fontSize: '30px'}}>
                   Life at Serambi Buku
             </h1>
-            <p className="text-justify">
-              ini join
-            </p>
-          </div>
+          <p className="text-justify">
+          ini join
+      </p>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default JoinNowPage;
