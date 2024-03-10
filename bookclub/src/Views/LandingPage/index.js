1ca4dadadf440b9c16e529925efac955ea738283
FadhelHudzafah <hudzafah20@gmail.com>
2024-03-10 19:01:53 +0700

    landingpage7

--- bookclub/src/Views/LandingPage/index.js
+++ bookclub/src/Views/LandingPage/index.js
@@ -263,7 +263,7 @@ const LandingPage = () => {
           </motion.div>
         </div>
         <div
-          className="carousel-item h-screen w-screen bg-gradient-to-b from-[#E3C4CC] from-95% to-[#B39373] flex"
+          className="carousel-item h-screen w-screen bg-gradient-to-b from-[#FFF3C7] from-95% to-[#FFF3C7] flex"
           id="section-3"
           ref={ref3}
         >
@@ -273,30 +273,101 @@ const LandingPage = () => {
             initial="hidden"
             animate={control3}
           >
-            <div className="hero-content flex-col lg:flex-row">
-              <img
-                src="images/stamp.gif"
-                className="max-w-lg sm:max-w-sm sm:max-h-sm rounded-lg"
-                alt="Stamp GIF"
-              />
-              <div className="md:pl-12 animate-fade-left">
-                <h1 className="sm:max-w-sm sm:max-h-sm text-5xl font-bold">
-                  Single Stamp Feature!
-                </h1>
-                <p className="sm:max-w-sm sm:max-h-sm py-6 text-justify">
-                  Selain itu, kami memberikan kemampuan untuk melakukan stempel
-                  e-materai, baik untuk satu dokumen atau dalam jumlah besar
-                  (bulk document). Ini memungkinkan pengguna untuk memberikan
-                  cap digital e-materai pada dokumen secara mudah dan efisien,
-                  memberikan fleksibilitas dalam penanganan proses stempel pada
-                  satu dokumen atau sekaligus pada sejumlah besar dokumen.
-                </p>
+            
+            <div className="mt-40 sm:mt-16">
+              <div style={{ 
+                width: '1000px', 
+                height: '250px', 
+                backgroundColor: '#C8AE7D', 
+                marginTop: '10px', 
+                position: "sticky",
+                borderRadius: '20px' }}>  
+              </div>
+              <h1 className="text-4xl font-bold animate-fade-up text-center" style={{ fontFamily: 'Tinos', color:"#484848", fontSize: '30px', marginTop: '10px', position: 'sticky'}}>
+                  Our Media Partner
+              </h1>
+              <div className="hero-content flex-col lg:flex-row">
+                <img
+                  src="images/cnbc.png"
+                  className="max-w-lg sm:max-w-sm sm:max-h-sm rounded-lg"
+                  alt="Stamp GIF"
+                  style={{ width: '240px', height: '120px', marginLeft: '-90px' }}
+                />
+                <img
+                  src="images/gramedia.png"
+                  className="max-w-lg sm:max-w-sm sm:max-h-sm rounded-lg"
+                  alt="Stamp GIF"
+                  style={{ width: '130px', height: '120px', marginRight: '10px' }}
+                />
+                <img
+                  src="images/mandiri.png"
+                  className="max-w-lg sm:max-w-sm sm:max-h-sm rounded-lg"
+                  alt="Stamp GIF"
+                  style={{ width: '160px', height: '120px', marginRIght: '20px' }}
+                />
+                <img
+                  src="images/tokopedia.png"
+                  className="max-w-lg sm:max-w-sm sm:max-h-sm rounded-lg"
+                  alt="Stamp GIF"
+                  style={{ width: '140px', height: '120px', marginRight: '20px' }}
+                />
+                <img
+                  src="images/unnes.png"
+                  className="max-w-lg sm:max-w-sm sm:max-h-sm rounded-lg"
+                  alt="Stamp GIF"
+                  style={{ width: '100px', height: '120px', marginLeft: '10px' }}
+                />
               </div>
             </div>
+            <div className="hero-content flex-col lg:flex-row">
+              <pre className="text-4xl font-bold animate-fade-up" style={{ fontFamily: 'Tinos', color:"#484848", fontSize: '30px', marginTop: '-300px', position: 'sticky', marginRight: '200px'}}>
+                By The
+                Number
+              </pre>
+              <pre className="text-4xl font-bold animate-fade-up" style={{ fontFamily: 'Tinos', color:"#484848", fontSize: '30px', marginTop: '-300px', position: 'sticky', marginRight: '200px'}}>
+                5.900+
+              </pre>
+              <pre className="text-4xl font-bold animate-fade-up" style={{ fontFamily: 'Tinos', color:"#484848", fontSize: '30px', marginTop: '-200px', position: 'sticky', marginRight: '150px'}}>
+                1.000+
+              </pre>
+            </div>
+            <div className="hero-content flex-col lg:flex-row">
+              <pre className="text-4xl animate-fade-up" style={{ fontFamily: 'Tinos', color:"#484848", fontSize: '15px', marginTop: '-230px', position: 'sticky', marginRight: '130px'}}>
+                Founded in 2015,
+              </pre>
+              <pre className="sm:max-w-sm sm:max-h-sm py-6 text-justify" style={{ fontFamily: 'Tinos', color:"#484848", fontSize: '15px', marginTop: '-250px', position: 'sticky', marginRight: '-90px'}}>
+                Active Members listed in our 
+                community until now.
+              </pre>
+              <pre className="sm:max-w-sm sm:max-h-sm py-6 text-justify" style={{ fontFamily: 'Tinos', color:"#484848", fontSize: '15px', marginTop: '-150px', position: 'sticky', marginLeft: '30px'}}>
+              Partners collaborating to create 
+              activities, events, and contents.
+              </pre>
+            </div>
+            <div className="hero-content flex-col lg:flex-row">
+              <pre className="text-4xl font-bold animate-fade-up" style={{ fontFamily: 'Tinos', color:"#484848", fontSize: '30px', marginTop: '-90px', position: 'sticky', marginLeft: '250px'}}>
+                93.000+
+              </pre>
+              <pre className="text-4xl font-bold animate-fade-up" style={{ fontFamily: 'Tinos', color:"#484848", fontSize: '30px', marginTop: '-10px', position: 'sticky', marginLeft: '220px'}}>
+                52+
+              </pre>
+            </div>
+            <div className="hero-content flex-col lg:flex-row">
+              <pre className="sm:max-w-sm sm:max-h-sm py-6 text-justify" style={{ fontFamily: 'Tinos', color:"#484848", fontSize: '15px', marginTop: '-40px', position: 'sticky', marginLeft: '300px'}}>
+                Audience in our social media.
+              </pre>
+              <pre className="sm:max-w-sm sm:max-h-sm py-6 text-justify" style={{ fontFamily: 'Tinos', color:"#484848", fontSize: '15px', marginTop: '40px', position: 'sticky', marginLeft: '70px'}}>
+                Active Volunteers running our community.
+              </pre>
+            </div>
+            
+              
+            
+            
           </motion.div>
         </div>
         <div
-          className="carousel-item h-screen w-screen bg-gradient-to-b from-[#B39373] from-95% to-[#B9DEAA] flex"
+          className="carousel-item h-screen w-screen bg-gradient-to-b from-[#FFF3C7] from-95% to-[#FFF3C7] flex"
           id="section-4"
           ref={ref4}
         >
