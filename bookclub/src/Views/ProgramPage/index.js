96c561e259b87984f73262ba93d55771a26748db
fanina huwaida <fanina0105@gmail.com>
2024-04-30 15:28:27 +0700

    freebies unfinished

--- bookclub/src/Views/ProgramPage/index.js
+++ bookclub/src/Views/ProgramPage/index.js
@@ -1,26 +1,78 @@
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
-function ProgramPage() {
+const boxVariant = {
+  visible: { opacity: 1, scale: 1, transition: { duration: 1 } },
+  hidden: { opacity: 0, scale: 0 },
+};
+
+const LandingPage = () => {
+  const control1 = useAnimation();
+  const [ref1, inView1] = useInView();
+
+  const control2 = useAnimation();
+  const [ref2, inView2] = useInView();
+
+  const control3 = useAnimation();
+  const [ref3, inView3] = useInView();
+
+  const control4 = useAnimation();
+  const [ref4, inView4] = useInView();
+
+  const control5 = useAnimation();
+  const [ref5, inView5] = useInView();
+
+  useEffect(() => {
+    if (inView1) {
+      control1.start("visible");
+    }
+  }, [control1, inView1]);
+  useEffect(() => {
+    if (inView2) {
+      control2.start("visible");
+    }
+  }, [control2, inView2]);
+  useEffect(() => {
+    if (inView3) {
+      control3.start("visible");
+    }
+  }, [control3, inView3]);
+  useEffect(() => {
+    if (inView4) {
+      control4.start("visible");
+    }
+  }, [control4, inView4]);
+  useEffect(() => {
+    if (inView5) {
+      control5.start("visible");
+    }
+  }, [control5, inView5]);
   return (
-    <>
-      <div className="navbar flex bg-rose-500 fixed z-10 rounded-b-lg w-full">
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1, x: 20 }}
-          transition={{ ease: "easeOut", duration: 1 }}
-          className="container flex justify-center"
+    <div className="HomePage">
+      <div className="navbar flex fixed z-10 rounded-b-lg w-full" style={{ backgroundColor: '#C8AE7D' }}>
+            <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, x: 20 }}
+            transition={{ ease: "easeOut", duration: 1 }}
+            className="container flex justify-center"
         >
-          <motion.a
+            <motion.a
             href="/About"
-            className="px-2 group hover:text-white"
+            className="relative px-2 group hover:text-white"
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 1.3 }}
             transition={{ ease: "easeOut", duration: 0.3 }}
-          >
+            style={{fontFamily: 'kaisei tokumin'}}
+            >
             About
-            <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-white"></span>
-          </motion.a>
+            <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-white"></span>
+            <div class="absolute left-0 mt-2 w-40 bg-white rounded-lg shadow-lg hidden group-hover:block">
+                <a href="/About/VisiMisi" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">Visi Misi</a>
+                <a href="/About/LifeAtBookClub" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">Life At Book Club</a>
+                <a href="/Vision" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">Vision</a>
+            </div>
+            </motion.a>
           <div className="divider divider-horizontal bg-white w-auto"></div>
           <motion.a
             href="/Program"
@@ -28,9 +80,14 @@ function ProgramPage() {
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 1.3 }}
             transition={{ ease: "easeOut", duration: 0.3 }}
+            style={{fontFamily: 'kaisei tokumin'}}
           >
             Program
-            <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-white"></span>
+            <div class="absolute left-0 mt-2 w-40 bg-white rounded-lg shadow-lg hidden group-hover:block">
+                <a href="/Program/BookTalk" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">BookTalk, Discussion, Bootcamp</a>
+                <a href="/Program/Competition" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">Competition</a>
+                <a href="/Program/" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">Freebies</a>
+            </div>
           </motion.a>
           <div className="divider divider-horizontal bg-white w-auto"></div>
           <motion.a
@@ -39,9 +96,10 @@ function ProgramPage() {
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 1.3 }}
             transition={{ ease: "easeOut", duration: 0.3 }}
+            style={{fontFamily: 'kaisei tokumin'}}
           >
             Update
-            <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-white"></span>
+            <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-white"></span>
           </motion.a>
           <div className="divider divider-horizontal bg-white w-auto"></div>
           <motion.a
@@ -50,9 +108,10 @@ function ProgramPage() {
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 1.3 }}
             transition={{ ease: "easeOut", duration: 0.3 }}
+            style={{fontFamily: 'kaisei tokumin'}}
           >
             Join Now
-            <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-white"></span>
+            <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-white"></span>
           </motion.a>
           <div className="divider divider-horizontal bg-white w-auto"></div>
           <motion.a
@@ -61,32 +120,152 @@ function ProgramPage() {
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 1.3 }}
             transition={{ ease: "easeOut", duration: 0.3 }}
+            style={{fontFamily: 'kaisei tokumin'}}
           >
-            Home
+            <img 
+            src="images/logo1.2.png" 
+            alt="Home" 
+            style={{width: "80px", height: "auto"}} />
             <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-white"></span>
           </motion.a>
-
         </motion.div>
       </div>
-      <div className="App">
-        <div className="hero-content flex-col lg:flex-col">
-          <img
-            src="images/upload.gif"
-            className="max-w-lg sm:max-w-sm sm:max-h-sm sm:mt-8 rounded-lg"
-            alt="Upload GIF"
-          />
-          <div className="mt-40 sm:mt-16">
-            <h1 className="sm:max-w-sm sm:max-h-sm text-5xl font-bold">
-              Program
-            </h1>
-            <p className="sm:max-w-sm sm:max-h-sm py-6 text-justify">
-              Ini Program
-            </p>
-          </div>
+      <div className="max-h-screen carousel carousel-vertical max-w-screen flex flex-col pt-16">
+        <div
+          className="carousel-item h-screen w-screen bg-gradient-to-b from-[#FFF3C7] from-80% to-[#FFF3C7] flex"
+          id="section-1"
+          ref={ref1}
+        >
+          <motion.div
+            className="hero min-h-screen"
+            variants={boxVariant}
+            initial="hidden"
+            animate={control1}
+          >
+            <div className="hero-content flex-col lg:flex-row" style={{ marginTop: '-100px' }}>
+              <div className="md:pr-20 animate-fade-left" style={{ flex: 30 }}>
+                <h1 className="mb-5 mr-80 m:max-w-sm sm:max-h-sm text-4xl font-bold animate-fade-up" style={{ fontFamily: 'kaisei tokumin', color: "#484848", fontSize: '30px', marginLeft: '-35px' }}>
+                  Freebies
+                </h1>
+                <div className="w-[30%] sm:w-[50%] ml-5 mt-7"> 
+                  <div className="flex mt-7 w-full justify-between">
+                    <div className="w-[30%] sm:w-[30%] mr-80">
+                      <div className="card677777 bg-gradient-to-b from-[#C8AE7D] from-95% to-[#C8AE7D]  shadow-xl animate-fade-up" style={{ width: '245px', height: '355px', borderRadius: '29px', marginLeft: '-25px' }}>
+                      <figure className="px-4 pt-5">
+                      <img src="/images/update1.jpg" 
+                      alt="Shoes" 
+                      style={{ width: '245px', height: '230px', borderRadius: '24px' }}
+                      />
+                      </figure>
+                      <div className="card-body text-justify" style={{ marginTop: '-26px', position: 'relative' }}> </div>
+                        <h1 className="card-title pt-2" style={{ fontFamily: 'Tinos', color:"#484848", fontSize: '16px', marginTop: '-12px', wordSpacing: '1px', marginLeft: '15px' }}>
+                          2024 Freebies: Printables & Phone Wallpapers
+                        </h1>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+                <div className="w-[30%] sm:w-[30%] ml-75 mr-100">
+                  <div className="card677777 bg-gradient-to-b from-[#C8AE7D] from-95% to-[#C8AE7D]  shadow-xl animate-fade-up" style={{ width: '245px', height: '355px', borderRadius: '29px', marginLeft: '-350px', marginTop: '7dfffffffffffgvvvvvvvvvvvv0px'}}>
+                    <figure className="px-4 pt-5">
+                      <img src="/images/update2.jpg" 
+                      alt="Shoes" 
+                      style={{ width: '245px', height: '194px', borderRadius: '24px' }}
+                      />
+                    </figure>
+                    <div className="card-body text-justify" style={{ marginTop: '-26px', position: 'relative' }}> </div>
+                      <h1 className="card-title pt-2" style={{ fontFamily: 'Tinos', color:"#484848", fontSize: '16px', marginTop: '-12px', wordSpacing: '1px', marginLeft: '15px' }}>
+                        2023 Freebies: Printables & Phone Wallpapers
+                      </h1>
+                  </div>
+                </div>
+                <div className="w-[30%] sm:w-[30%] ml-75 mr-100">
+                  <div className="card677777 bg-gradient-to-b from-[#C8AE7D] from-95% to-[#C8AE7D]  shadow-xl animate-fade-up" style={{ width: '245px', height: '355px', borderRadius: '29px', marginLeft: '-30px', marginTop: '20'}}>
+                    <figure className="px-4 pt-5">
+                      <img src="/images/update2.jpg" 
+                      alt="Shoes" 
+                      style={{ width: '245px', height: '194px', borderRadius: '24px' }}
+                      />
+                    </figure>
+                    <div className="card-body text-justify" style={{ marginTop: '-26px', position: 'relative' }}> </div>
+                      <h1 className="card-title pt-2" style={{ fontFamily: 'Tinos', color:"#484848", fontSize: '16px', marginTop: '-12px', wordSpacing: '1px', marginLeft: '15px' }}>
+                        2022 Freebies: Printables & Phone Wallpapers
+                      </h1>
+                  </div>
+                </div>
+            </div>
+               
+          </motion.div>
+        </div>
+
+        <div
+          className="carousel-item h-screen w-screen bg-[#FFF3C7] flex"
+          id="section-5"
+          ref={ref2}
+        >
+          <motion.div
+            className="hero min-h-screen"
+            variants={boxVariant}
+            initial="hidden"
+            animate={control5}
+          >
+            <div className="mt-40 sm:mt-16">
+              <div style={{ 
+                width: '1000px', 
+                height: '240px', 
+                backgroundColor: '#C8AE7D', 
+                marginTop: '-220px', 
+                position: "sticky",
+                borderRadius: '20px' }}>  
+                <a
+                  href="/JoinNow"
+                  className="btn transition ease-in-out delay-50 bg-[#FFF3C7] shadow-xl text-black border-none hover:-translate-y-1 hover:scale-110 hover:bg-[#63CFF9] hover:text-white hover:border-white duration-500 ..."
+                  style={{ color:"#484848", marginTop:'150px', marginLeft:'700px' }}
+                  >
+                    Subscribe to Our Channel
+                  </a>
+              </div>
+              
+            </div>
+            <pre className="text-4xl font-bold animate-fade-up" style={{ fontFamily: 'Kaisei Tokumin', color:"#484848", fontSize: '40px', marginTop: '-300px', position: 'sticky', marginRight: '450px'}}>
+                Subscribe To Our Update!
+            </pre>
+            <pre className="text-4xl font-bold animate-fade-up" style={{ fontFamily: 'Kaisei Tokumin', color:"#484848", fontSize: '17px', marginTop: '-200px', position: 'sticky', marginRight: '550px'}}>
+                Get new content delivered directly to your inbox.
+            </pre>
+            
+            <footer className="footer p-10 bg-[#222831] text-base-content" style= {{marginBottom:'-335px'}}>
+                <aside>
+                  <img src="images/logo1.png" style={{ width: '120px', height: '120px', marginTop:'-30px'}}></img>
+                <p>Serambi Buku<br/>Founded since 2015</p>
+                </aside> 
+                <nav>
+                  <h6 className="footer-title">Services</h6> 
+                  <a className="link link-hover">Branding</a>
+                  <a className="link link-hover">Design</a>
+                  <a className="link link-hover">Marketing</a>
+                  <a className="link link-hover">Advertisement</a>
+                </nav> 
+                <nav>
+                <h6 className="footer-title">Company</h6> 
+                <a className="link link-hover">About us</a>
+                <a className="link link-hover">Contact</a>
+                <a className="link link-hover">Jobs</a>
+                <a className="link link-hover">Press kit</a>
+                </nav> 
+                <nav>
+                <h6 className="footer-title">Legal</h6> 
+                <a className="link link-hover">Terms of use</a>
+                <a className="link link-hover">Privacy policy</a>
+                <a className="link link-hover">Cookie policy</a>
+                </nav>
+              </footer>
+          </motion.div>
         </div>
       </div>
-    </>
+    </div>
   );
-}
+};
 
-export default ProgramPage;
+export default LandingPage;
