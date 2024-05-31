71d8542323e918ad68aea816051c68019605d79f
FadhelHudzafah <hudzafah20@gmail.com>
2024-05-31 17:39:07 +0700

    updatepadelevent4

--- bookclub/src/App.js
+++ bookclub/src/App.js
@@ -43,8 +43,8 @@ class App extends Component {
             <Route exact path="/Program/Booktalk/CompetitionNews" element={<CompetitionNewsPage />} />
             <Route exact path="/Program/Booktalk/EventFlores" element={<EventFloresPage />} />
             <Route exact path="/Program/Booktalk/EventMemori" element={<EventMemoriPage />} />
-            <Route exact path="/Program/Update/EventUpdate1" element={<EventUpdate1Page/>} />
-            <Route exact path="/Program/Update/EventUpdate2" element={<EventUpdate2Page/>} />
+            <Route exact path="/Update/EventUpdate1" element={<EventUpdate1Page/>} />
+            <Route exact path="/Update/EventUpdate2" element={<EventUpdate2Page/>} />
 
           </Routes>
         </div>
